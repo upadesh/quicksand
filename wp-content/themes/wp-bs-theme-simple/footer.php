5dac667675269de8633c8d6ebaba36b27721729f
Piccard <piccard@web.de>
2016-09-25 09:46:01 +0200

    wp_footer() for adminbar added

--- wp-content/themes/wp-bs-theme-simple/footer.php
+++ wp-content/themes/wp-bs-theme-simple/footer.php
@@ -11,81 +11,82 @@
 ?>
 
 
-            </div><!-- site-content-->
+</div><!-- site-content-->
 
 
-            <!-- site-footer -->
-            <footer class="site-footer">  
-                <!-- site-widgetbar -->
-                <div class="site-widgetbar">
-                    <div class="row">
-                        <div class="site-widget-item text-xs-center">
-                            <ul class="list-group">
-                                <li class="list-group-item">
-                                    <span class="tag tag-default tag-pill pull-xs-right">14</span>
-                                    Cras justo odio
-                                </li>
-                                <li class="list-group-item">
-                                    <span class="tag tag-default tag-pill pull-xs-right">2</span>
-                                    Dapibus ac facilisis in
-                                </li>
-                                <li class="list-group-item">
-                                    <span class="tag tag-default tag-pill pull-xs-right">1</span>
-                                    Morbi leo risus
-                                </li>
-                            </ul>
-                        </div> 
-                        <div class="site-widget-item text-xs-center"></div>  
-                        <div class="site-widget-item text-xs-center"></div>  
-                        <div class="site-widget-item text-xs-center"></div>  
-                    </div><!-- .row -->
-                </div><!-- .site-widgetbar -->  
-            </footer><!-- .site-footer -->
+<!-- site-footer -->
+<footer class="site-footer">  
+    <!-- site-widgetbar -->
+    <div class="site-widgetbar">
+        <div class="row">
+            <div class="site-widget-item text-xs-center">
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <span class="tag tag-default tag-pill pull-xs-right">14</span>
+                        Cras justo odio
+                    </li>
+                    <li class="list-group-item">
+                        <span class="tag tag-default tag-pill pull-xs-right">2</span>
+                        Dapibus ac facilisis in
+                    </li>
+                    <li class="list-group-item">
+                        <span class="tag tag-default tag-pill pull-xs-right">1</span>
+                        Morbi leo risus
+                    </li>
+                </ul>
+            </div> 
+            <div class="site-widget-item text-xs-center"></div>  
+            <div class="site-widget-item text-xs-center"></div>  
+            <div class="site-widget-item text-xs-center"></div>  
+        </div><!-- .row -->
+    </div><!-- .site-widgetbar -->  
+</footer><!-- .site-footer -->
 
-        </div><!-- .site-main-container -->
+</div><!-- .site-main-container -->
 
 
-        <!-- site-info -->
-        <div class="container-fluid">
-            <div class="site-info">
-                <div class="row">
-                    <div class="site-social">
-                        <div class="text-xs-center text-lg-right"> 
-                            <ul class="list-inline">
-                                <li class="d-inline">
-                                    <a href="#">
-                                        <span class="fa-stack fa-lg">
-                                            <i class="fa fa-circle fa-stack-2x"></i>
-                                            <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
-                                        </span>
-                                    </a>
-                                </li>
-                                <li class="d-inline">
-                                    <a href="#">
-                                        <span class="fa-stack fa-lg">
-                                            <i class="fa fa-circle fa-stack-2x"></i>
-                                            <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-                                        </span>
-                                    </a>
-                                </li>
-                                <li class="d-inline">
-                                    <a href="#">
-                                        <span class="fa-stack fa-lg">
-                                            <i class="fa fa-circle fa-stack-2x"></i>
-                                            <i class="fa fa-github fa-stack-1x fa-inverse"></i>
-                                        </span>
-                                    </a>
-                                </li>
-                            </ul> 
-                        </div>
-                    </div>
-                    <div class="site-copyright ">
-                        <div class="site-info text-xs-center text-lg-left">
-                            <p class="text-muted lead">Copyright &copy; <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>2016</p>
-                        </div>
-                    </div>
+<!-- site-info -->
+<div class="container-fluid">
+    <div class="site-info">
+        <div class="row">
+            <div class="site-social">
+                <div class="text-xs-center text-lg-right"> 
+                    <ul class="list-inline">
+                        <li class="d-inline">
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li class="d-inline">
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li class="d-inline">
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                    </ul> 
                 </div>
-            </div><!-- site-info -->
+            </div>
+            <div class="site-copyright ">
+                <div class="site-info text-xs-center text-lg-left">
+                    <p class="text-muted lead">Copyright &copy;2016 <a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
+                </div>
+            </div>
         </div>
-    </body>
+    </div><!-- site-info -->
+</div>
+<?php wp_footer(); ?>
+</body>
 </html>
