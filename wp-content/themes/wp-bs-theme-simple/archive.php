8fa187c5c7f69a4defa5d2f10e19a28945548446
Piccard <piccard@web.de>
2016-09-27 17:29:22 +0200

    archive

--- wp-content/themes/wp-bs-theme-simple/archive.php
+++ wp-content/themes/wp-bs-theme-simple/archive.php
@@ -24,49 +24,45 @@ get_header();
     <!--  site-content-area -->  
     <main id="primary" class="site-content-area">  
 
-        <?php 
-        if (have_posts()) : ?>
+        <?php if (have_posts()) : ?>
 
             <header class="page-header">
-                <?php
-                the_archive_title('<h1 class="page-title">', '</h1>');
-                the_archive_description('<div class="taxonomy-description">', '</div>');
-                ?>
+    <?php
+    the_archive_title('<h1 class="page-title">', '</h1>');
+    the_archive_description('<div class="taxonomy-description">', '</div>');
+    ?>
             </header><!-- .page-header -->
 
-            <?php
-            // Start the Loop.
-            while (have_posts()) : the_post();
+    <?php
+    // Start the Loop.
+    while (have_posts()) : the_post();
 
-                /*
-                 * Include the Post-Format-specific template for the content.
-                 * If you want to override this in a child theme, then include a file
-                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                 */
-                get_template_part('template-parts/content', get_post_format());
+        /*
+         * Include the Post-Format-specific template for the content.
+         * If you want to override this in a child theme, then include a file
+         * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+         */
+        get_template_part('template-parts/content', get_post_format());
 
-            // End the loop.
-            endwhile;
+    // End the loop.
+    endwhile;
 
-            // Previous/next post navigation. 
-            the_post_navigation(array(
-                'next_text' => '<span class="meta-nav" aria-hidden="true">' . __('Next', 'wp-bs-theme-simple') . '</span> ' .
-                '<span class="screen-reader-text">' . __('Next post:', 'wp-bs-theme-simple') . '</span> ' .
-                '<span class="post-title">%title</span>',
-                'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __('Previous', 'wp-bs-theme-simple') . '</span> ' .
-                '<span class="screen-reader-text">' . __('Previous post:', 'wp-bs-theme-simple') . '</span> ' .
-                '<span class="post-title">%title</span>',
-            ));
+    // Previous/next page navigation.
+    the_posts_pagination(array(
+        'prev_text' => __('Previous page', 'twentysixteen'),
+        'next_text' => __('Next page', 'twentysixteen'),
+        'before_page_number' => '<span class="meta-nav screen-reader-text">' . __('Page', 'twentysixteen') . ' </span>',
+    ));
 
-        // If no content, include the "No posts found" template.
-        else :
-            get_template_part('template-parts/content', 'none');
+// If no content, include the "No posts found" template.
+else :
+    get_template_part('template-parts/content', 'none');
 
-        endif;
-        ?>
+endif;
+?>
 
     </main><!--  .site-content-area --> 
-    <?php get_sidebar(); ?>
+<?php get_sidebar(); ?>
 </div> 
 
 <?php get_footer(); ?>
--- wp-content/themes/wp-bs-theme-simple/template-parts/content-page.php
+++ wp-content/themes/wp-bs-theme-simple/template-parts/content-page.php
@@ -3,8 +3,8 @@
  * The template used for displaying page content
  *
  * @package WordPress
- * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
+ * @subpackage wp-bs-theme-simple
+ * @since WP-bs-theme-simple 0.0.1
  */
 ?>
 
