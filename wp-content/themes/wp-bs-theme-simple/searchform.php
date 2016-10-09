4bd20ae3a383bd63159c161f527cec0c8ee8ae78
Piccard <piccard@web.de>
2016-10-09 17:22:34 +0200

    bs-pagination

--- wp-content/themes/wp-bs-theme-simple/searchform.php
+++ wp-content/themes/wp-bs-theme-simple/searchform.php
@@ -12,7 +12,7 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
     <div class="input-group">
         <div class="input-group">
-            <input type="search" class="form-control" placeholder="<?php echo _x( 'Search for:', 'label', 'wp-bs-theme-simple' ); ?>" value="<?php echo get_search_query(); ?>" name="s" >
+            <input type="search" class="form-control" placeholder="<?php echo _x( '...', 'label', 'wp-bs-theme-simple' ); ?>" value="<?php echo get_search_query(); ?>" name="s" >
             <span class="input-group-btn"> 
                 <button class="btn btn-secondary" type="submit">
                     <span class=""><?php echo _x( 'Search', 'submit button', 'wp-bs-theme-simple' ); ?></span>
