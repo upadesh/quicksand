8f5b8f795830eb77965f3da397a9a76d64830c20
Piccard <piccard@web.de>
2016-10-17 20:19:11 +0200

    widgets lists are now list-groups

--- wp-content/themes/quicksand/functions.php
+++ wp-content/themes/quicksand/functions.php
@@ -322,7 +322,7 @@ if (!function_exists('quicksand_widgets_init')) :
         register_sidebar(array(
             'name' => __('Sidebar-Footer', 'quicksand'),
             'id' => 'sidebar-2',
-            'description' => __('Appears at the bottom of the content on posts and pages.', 'quicksand'),
+            'description' => __('Add widgets here to appear at the bottom.', 'quicksand'),
             'before_widget' => '<section id="%1$s" class="widget %2$s">',
             'after_widget' => '</section>',
             'before_title' => '<h2 class="widget-title">',
