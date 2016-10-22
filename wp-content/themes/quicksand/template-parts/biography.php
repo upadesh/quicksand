56eed6f96245ba94d00068db844fa074fcb972fd
Piccard <piccard@web.de>
2016-10-22 16:59:30 +0200

    content card-block

--- wp-content/themes/quicksand/template-parts/biography.php
+++ wp-content/themes/quicksand/template-parts/biography.php
@@ -9,34 +9,32 @@
 ?> 
 
 
-<div class="list-group list-group-flush author-bio"> 
-        <li class="list-group-item"> 
-            <?php
-            /**
-             * Filter the Quicksand author bio avatar size.
-             *
-             * @since Quicksand 0.2.1
-             *
-             * @param int $size The avatar height and width size in pixels.
-             */
-            $author_bio_avatar_size = apply_filters('quicksand_author_bio_avatar_size', 42);
+<div class="card-block author-bio">  
+    <?php
+    /**
+     * Filter the Quicksand author bio avatar size.
+     *
+     * @since Quicksand 0.2.1
+     *
+     * @param int $size The avatar height and width size in pixels.
+     */
+    $author_bio_avatar_size = apply_filters('quicksand_author_bio_avatar_size', 42);
 
-            echo get_avatar(get_the_author_meta('user_email'), $author_bio_avatar_size);
-            ?>
-            <!-- end avatar -->
+    echo get_avatar(get_the_author_meta('user_email'), $author_bio_avatar_size);
+    ?>
+    <!-- end avatar -->
 
-            <!-- user bio -->
-            <div class="author-bio-content">
+    <!-- user bio -->
+    <div class="author-bio-content">
 
-                <h4 class="card-title author-name"><?php echo get_the_author(); ?></h4>
-                <p class="card-text author-description"> 
-                    <?php the_author_meta('description'); ?>
-                </p>  
-                <p>
-                    <a class="card-link author-link" href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>" rel="author">
-                        <?php printf(__('View all posts by %s', 'quicksand'), get_the_author()); ?>
-                    </a>
-                </p>
-            </div><!-- end .author-bio-content -->
-        </li> 
+        <h4 class="card-title author-name"><?php echo get_the_author(); ?></h4>
+        <p class="card-text author-description"> 
+            <?php the_author_meta('description'); ?>
+        </p>  
+        <p>
+            <a class="card-link author-link" href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>" rel="author">
+                <?php printf(__('View all posts by %s', 'quicksand'), get_the_author()); ?>
+            </a>
+        </p>
+    </div><!-- end .author-bio-content --> 
 </div>   
--- wp-content/themes/quicksand/template-parts/content.php
+++ wp-content/themes/quicksand/template-parts/content.php
@@ -2,13 +2,15 @@
 <!-- post --> 
 <article class="card" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>  
 
-    <!-- .entry-header --> 
+    <!-- post-title --> 
     <header class="entry-header card-block">
+        
+        <!--stick post-->
         <?php if (is_sticky() && is_home() && !is_paged()) : ?>
             <span class="sticky-post"><?php _e('Featured', 'quicksand'); ?></span>
         <?php endif; ?>
 
-        <!-- .entry-title -->   
+        <!-- post-title -->   
         <?php
         the_title(sprintf('<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h1>');
         ?> 
@@ -23,20 +25,8 @@
     <!--post thumbnail-->
     <?php quicksand_entry_thumbnail(); ?> 
 
-    <!-- .entry-content --> 
-    <div class="entry-content">   
-        <?php
-        /* translators: %s: Name of current post */
-        the_content(sprintf(
-                        __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'quicksand'), get_the_title())
-        );
-
-        /* Displays page links for paginated posts (i.e. includes the <!–nextpage–>) */ 
-        quicksand_paginated_posts_paginator();
-        ?> 
-    </div><!-- .entry-content -->   
-
-
+    <!--post-content--> 
+    <?php quicksand_single_entry_content(); ?> 
 
     <!-- edit-link --> 
     <?php quicksand_entry_footer(); ?> 
