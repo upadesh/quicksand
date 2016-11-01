a8a6d2213b24c94de177f00f9571c29cb71f46f2
Piccard <piccard@web.de>
2016-11-01 02:21:56 +0100

    index masonry

--- wp-content/themes/quicksand/index.php
+++ wp-content/themes/quicksand/index.php
@@ -33,6 +33,12 @@ get_header();
                 <?php
             endif;
 
+            // show posts in masonry-style 
+            if (!is_single() && !is_attachment() && !is_page() && get_theme_mod('qs_content_masonry', quicksand_get_color_scheme()['settings']['qs_content_masonry'])) {
+                ?><div class="card-columns"> <?php
+            }
+            
+
             while (have_posts()) : the_post();
 
                 /*
@@ -45,7 +51,13 @@ get_header();
                  */
                 get_template_part('template-parts/content', get_post_format());
 
-            endwhile; 
+            endwhile;  
+            
+            // show posts in masonry-style
+            if (get_theme_mod('qs_content_masonry', quicksand_get_color_scheme()['settings']['qs_content_masonry'])) {
+                ?></div> <?php
+            }
+
             
             // pagination
             quicksand_bs_style_paginator();
