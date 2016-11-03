495018fdca0196126df99565e16483085d3650bc
Piccard <piccard@web.de>
2016-11-03 22:15:50 +0100

    tag modified

--- wp-content/themes/quicksand/archive.php
+++ wp-content/themes/quicksand/archive.php
@@ -24,36 +24,37 @@ get_header();
     <!--  site-content-area -->  
     <main id="primary" class="site-content-area">  
 
-        <?php if (have_posts()) : 
+        <?php
+        if (have_posts()) :
             echo the_archive_title('<article class="card"><div class="card-block"><h4 class="card-title">', '</h4></article>');
-?>
+            ?>
             <!--show posts in masonry-style-->
-            <?php 
+            <?php
             if (get_theme_mod('qs_content_masonry', quicksand_get_color_scheme()['settings']['qs_content_masonry'])) {
                 ?><div class="card-columns"> <?php
+                }
+
+                while (have_posts()) : the_post();
+
+                    /*
+                     * Include the Post-Format-specific template for the content.
+                     * If you want to override this in a child theme, then include a file
+                     * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                     */
+                    get_template_part('template-parts/content', get_post_format());
+
+                // End the loop.
+                endwhile;
+
+                // show posts in masonry-style
+                if (get_theme_mod('qs_content_masonry', quicksand_get_color_scheme()['settings']['qs_content_masonry'])) {
+                    ?></div> <?php
             }
-            
-            while (have_posts()) : the_post();
 
-                /*
-                 * Include the Post-Format-specific template for the content.
-                 * If you want to override this in a child theme, then include a file
-                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                 */
-                get_template_part('template-parts/content', get_post_format());
 
-            // End the loop.
-            endwhile; 
-            
-            // show posts in masonry-style
-            if (get_theme_mod('qs_content_masonry', quicksand_get_color_scheme()['settings']['qs_content_masonry'])) {
-                ?></div> <?php
-            }
-
-            
             // pagination
             quicksand_bs_style_paginator();
-            
+
         // If no content, include the "No posts found" template.
         else :
             get_template_part('template-parts/content', 'none');
@@ -61,7 +62,7 @@ get_header();
         ?>
 
     </main><!--  .site-content-area --> 
-    <?php get_sidebar(); ?>
+<?php get_sidebar(); ?>
 </div> 
 
 <?php get_footer(); ?>
