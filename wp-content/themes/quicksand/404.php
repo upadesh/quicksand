80478e07cc66e4fc0d82e1219cfcdea3c507e6cf
Piccard <piccard@web.de>
2017-07-06 22:02:03 +0200

    esc_html_e

--- wp-content/themes/quicksand/404.php
+++ wp-content/themes/quicksand/404.php
@@ -20,13 +20,13 @@ get_header();
         <section class="error-404 not-found card">
             <header class="card-block page-header">
                 <h1 class="card-title page-title">
-                    <?php _e('Oops! That page can&rsquo;t be found.', 'quicksand'); ?>
+                    <?php esc_html_e('Oops! That page can&rsquo;t be found.', 'quicksand'); ?>
                 </h1>
-                <h6 class="card-subtitle text-muted"><?php _e('It looks like nothing was found at this location', 'quicksand'); ?></h6>
+                <h6 class="card-subtitle text-muted"><?php esc_html_e('It looks like nothing was found at this location', 'quicksand'); ?></h6>
             </header><!-- .page-header -->
 
             <div class="card-block page-content">
-                <p class="card-text"><?php _e('Maybe try a search?', 'quicksand'); ?></p> 
+                <p class="card-text"><?php esc_html_e('Maybe try a search?', 'quicksand'); ?></p> 
                 <?php get_search_form(); ?>
             </div><!-- .page-content -->
         </section><!-- .error-404 -->
