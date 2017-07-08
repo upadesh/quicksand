325465f22966b5fe2301bb30b07d363eeda9f7a1
Piccard <piccard@web.de>
2017-07-08 13:13:21 +0200

    escaping

--- wp-content/themes/quicksand/inc/class.QuicksandCustomizeCategoryControl.php
+++ wp-content/themes/quicksand/inc/class.QuicksandCustomizeCategoryControl.php
@@ -16,9 +16,31 @@ if (class_exists('WP_Customize_Control')) {
 
             $dropdown = str_replace('<select', '<select ' . $this->get_link(), $dropdown);
 
-            printf(
+//            printf(
+//                    '<label class="customize-control-select"><span class="customize-control-title">%s</span><span class="description customize-control-description">%s</span> %s</label>', $this->label, $this->description, $dropdown
+//            );
+            
+            $ctrl = sprintf(
                     '<label class="customize-control-select"><span class="customize-control-title">%s</span><span class="description customize-control-description">%s</span> %s</label>', $this->label, $this->description, $dropdown
             );
+            
+            echo wp_kses($ctrl, array(
+                'label' => array(),
+                'div' => array(),
+                'span' => array(
+                    'class' => array()
+                ),
+                'select' => array(
+                    'data-customize-setting-link' => array(),
+                    'name' => array(),
+                    'id' => array(),
+                    'class' => array()
+                ),
+                'option' => array(
+                    'value' => array(),
+                    'class' => array()
+                ),
+            ));
         }
 
     }
